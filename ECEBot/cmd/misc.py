c89a0220c672ec98fae95b152e63ddcb66e8233e
AbyxDev <ken@abyx.dev>
2022-11-28 11:45:36 -0500

    Remove invite command if INVITE_URL is None

--- ECEBot/cmd/misc.py
+++ ECEBot/cmd/misc.py
@@ -13,11 +13,12 @@ class Miscellaneous(commands.Cog):
         """Test whether the bot is running! Simply says "Hello World!"."""
         await ctx.response.send_message('Hello World!', ephemeral=True)
 
-    @app_commands.command()
-    @app_commands.default_permissions()
-    async def invite(self, ctx: discord.Interaction) -> None:
-        """Get an invite link for the bot."""
-        await ctx.response.send_message(INVITE_URL, ephemeral=True)
+    if INVITE_URL is not None:
+        @app_commands.command()
+        @app_commands.default_permissions()
+        async def invite(self, ctx: discord.Interaction) -> None:
+            """Get an invite link for the bot."""
+            await ctx.response.send_message(INVITE_URL, ephemeral=True)
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(Miscellaneous())
--- config.pyi
+++ config.pyi
@@ -1,7 +1,15 @@
 from typing import Optional
 
+# If running in a specific guild, set this to its ID.
+# Otherwise, use None.
 DEBUG_GUILD: Optional[int]
+# The Discord bot application token
 TOKEN: str
+# The log level to emit, e.g. logging.DEBUG
 LOG_LEVEL: int # logging.*
+# If True, logs will be emitted to standard output
+# instead of to a timestamped file.
 LOG_TO_STDOUT: bool
-INVITE_URL: str
+# An URL to give server admins to invite the bot.
+# Set to None to disable the /invite command.
+INVITE_URL: Optional[str]
