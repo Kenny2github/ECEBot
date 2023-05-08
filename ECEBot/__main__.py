36385ba2dea46676edec355b24c86f0e304542de
AbyxDev <ken@abyx.dev>
2023-05-08 00:32:47 -0400

    Need to add working directory to path

--- ECEBot/__main__.py
+++ ECEBot/__main__.py
@@ -1,8 +1,10 @@
 import asyncio
 import os
+import sys
 from pathlib import Path
 # parent directory of ECEBot
 os.chdir(Path(__file__).resolve().parent.parent)
+sys.path.append(os.getcwd())
 from ECEBot import done, run
 
 async def main():
